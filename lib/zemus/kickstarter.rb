a5f20001eea7a571dbae855a9d315ff40824002d
Jon Dodson <jon.dodson@livingsocial.com>
2013-10-16 18:43:18 -0700

    Adding Kickstarter embed

--- lib/zemus/kickstarter.rb
+++ lib/zemus/kickstarter.rb
@@ -1,9 +1,15 @@
 module Zemus
   class Kickstarter
 
-    def to_embed
-      "You're going to Kickstarter!"
+    def to_embed(url)
+      embed = "<iframe width='100%' height='' scrolling='no' src='http://www.kickstarter.com/projects/#{kickstarter_id(url)}/widget/video.html' frameborder='0'> </iframe>"
+      embed += "<a href='#{url}' target='_blank' class='btn btn-default btn-xs'><i class='glyphicon glyphicon-heart'></i> Back this on Kickstarter!</a>"
+
+      embed
     end
 
+    def kickstarter_id(url)
+      url.split("kickstarter.com/projects/").last.split("?").first
+    end
   end
 end
