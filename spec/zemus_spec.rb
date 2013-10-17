5d255fd5fdd17ed4fc8900dd9ae5fc8076cfdada
Jon Dodson <jon.dodson@livingsocial.com>
2013-10-16 19:40:37 -0700

    Acceptance test for Sound

--- spec/zemus_spec.rb
+++ spec/zemus_spec.rb
@@ -34,4 +34,10 @@ describe Zemus do
     end
   end
 
+  it "acceptance tests for sound" do
+    url = "http://www.podtrac.com/pts/redirect.mp3/content.duckfeed.tv/bsc/bsc_E001.mp3"
+
+    Zemus.embed(url).should eq("<audio controls='controls' class='media-object'><source src='http://www.podtrac.com/pts/redirect.mp3/content.duckfeed.tv/bsc/bsc_E001.mp3' type='audio/mpeg' /></audio><div><a href='http://www.podtrac.com/pts/redirect.mp3/content.duckfeed.tv/bsc/bsc_E001.mp3' target='_blank' class='img-thumbnail' download='bsc_E001.mp3'><i class='glyphicon glyphicon-music'></i> Download this podcast.</a></div>")
+  end
+
 end
