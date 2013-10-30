cd4f4d1b28e8b39f79bb803d8128049eae43a1d7
Jon Dodson <jon.dodson@livingsocial.com>
2013-10-29 19:56:23 -0700

    Fixing a bug with a kickstarter URL and upping the version number for release

--- lib/zemus/kickstarter.rb
+++ lib/zemus/kickstarter.rb
@@ -9,15 +9,20 @@ module Zemus
       @url = url
     end
 
+    def url
+      "http://www.kickstarter.com/projects/#{kickstarter_id}"
+    end
+
     def to_embed
       embed = "<iframe width='100%' height='600' scrolling='no' src='http://www.kickstarter.com/projects/#{kickstarter_id}/widget/video.html' frameborder='0'> </iframe>"
-      embed += "<a href='#{@url}' target='_blank' class='btn btn-default btn-xs'><i class='glyphicon glyphicon-heart'></i> Back this on Kickstarter!</a>"
+      embed += "<a href='#{url}' target='_blank' class='btn btn-default btn-xs'><i class='glyphicon glyphicon-heart'></i> Back this on Kickstarter!</a>"
 
       embed
     end
 
     def kickstarter_id
-      @url.split("kickstarter.com/projects/").last.split("?").first
+      id = @url.split("kickstarter.com/projects/").last.split("?").first
+      id.gsub("#", "")
     end
   end
 end
--- spec/zemus/kickstarter_spec.rb
+++ spec/zemus/kickstarter_spec.rb
@@ -4,5 +4,9 @@ describe Zemus::Kickstarter do
   it "embeds kickstarter" do
     Zemus::Kickstarter.new("http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake").to_embed.
       should eq("<iframe width='100%' height='600' scrolling='no' src='http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake/widget/video.html' frameborder='0'> </iframe><a href='http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake' target='_blank' class='btn btn-default btn-xs'><i class='glyphicon glyphicon-heart'></i> Back this on Kickstarter!</a>")
+  
+    # fix for a bug where a kickstarter URL had a # sign at the end
+    Zemus::Kickstarter.new("http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake#").to_embed.
+      should eq("<iframe width='100%' height='600' scrolling='no' src='http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake/widget/video.html' frameborder='0'> </iframe><a href='http://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake' target='_blank' class='btn btn-default btn-xs'><i class='glyphicon glyphicon-heart'></i> Back this on Kickstarter!</a>")
   end
 end
