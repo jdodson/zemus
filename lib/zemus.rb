899701955fc0cc34f7d7f251bd2ee55edcca14e0
Travis Newman <travis.c.newman@gmail.com>
2014-01-29 23:35:11 -0500

    added empty engine to serve up js and css, changed required files method in zemus.gemspec to allow for this

--- lib/zemus.rb
+++ lib/zemus.rb
@@ -3,6 +3,12 @@ Dir.glob(File.join(path, "/**/*.rb")).each { |f| require f }
 
 module Zemus
   
+  # This merely creates an empty engine to serve the assets
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+
   def self.embed(url)
     Zemus::Parser.build_embedder(url).to_embed
   end
