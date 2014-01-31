d4c31e654153851f2fc9e19b624c782b8625a631
Travis Newman <travis.c.newman@gmail.com>
2014-01-30 19:58:58 -0500

    Revert "put the assets back"
    This reverts commit 9e07a67b753f0d26bcde256ca9f518c2ea73b5a7.

--- lib/zemus/twitch.rb
+++ lib/zemus/twitch.rb
@@ -10,15 +10,16 @@ module Zemus
     end
 
     def to_embed
-      id = @url.split('/').last
       if     @url.split('/').fifth == "b"
+        id = @url.split('/').last
         chan = @url.split('/').fourth
-        "<div class='zemusflashembed'><object bgcolor='#000000' data='http://www.twitch.tv/widgets/archive_embed_player.swf' height='378' id='clip_embed_player_flash' type='application/x-shockwave-flash' width='620'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf'><param name='allowScriptAccess' value='always'><param name='allowNetworking' value='all'><param name='allowFullScreen' value='true'><param name='flashvars' value='channel=#{chan}&amp;auto_play=false&amp;start_volume=25&amp;archive_id=#{id}'></object><br><a href='http://www.twitch.tv/#{chan}' class='trk' style='padding:2px 0px 4px; display:block; width: 320px; font-weight:normal; font-size:10px; text-decoration:underline; text-align:center;'>Watch live video from #{chan} on TwitchTV</a></div>
+        "<div class='zemusflashembed'><object class='zemusflashembed' bgcolor='#000000' data='http://www.twitch.tv/widgets/archive_embed_player.swf' height='378' id='clip_embed_player_flash' type='application/x-shockwave-flash' width='620'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf'><param name='allowScriptAccess' value='always'><param name='allowNetworking' value='all'><param name='allowFullScreen' value='true'><param name='flashvars' value='channel=#{chan}&amp;auto_play=false&amp;start_volume=25&amp;archive_id=#{id}'></object><br><a href='http://www.twitch.tv/#{chan}' class='trk' style='padding:2px 0px 4px; display:block; width: 320px; font-weight:normal; font-size:10px; text-decoration:underline; text-align:center;'>Watch live video from #{chan} on TwitchTV</a></div>
         <div class='zemusnoflash'>Your browser cannot display this embedded Twitch stream.</div>"
         
       elsif  @url.split('/').fifth == "c"
+        id = @url.split('/').last
         chan = @url.split('/').fourth
-        "<div class='zemusflashembed'><object data='http://www.twitch.tv/widgets/archive_embed_player.swf'  id='clip_embed_player_flash' type='application/x-shockwave-flash'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf'><param name='allowScriptAccess' value='always'><param name='allowNetworking' value='all'><param name='allowFullScreen' value='true'><param name='flashvars' value='channel=#{chan}&amp;auto_play=false&amp;start_volume=25&amp;chapter_id=#{id}'></object><br><a href='http://www.twitch.tv/#{chan}' class='trk' style='padding:2px 0px 4px; display:block; width: 320px; font-weight:normal; font-size:10px; text-decoration:underline; text-align:center;'>Watch live video from #{chan} on TwitchTV</a></div>
+        "<div class='zemusflashembed'><object class='zemusflashembed'  data='http://www.twitch.tv/widgets/archive_embed_player.swf'  id='clip_embed_player_flash' type='application/x-shockwave-flash'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf'><param name='allowScriptAccess' value='always'><param name='allowNetworking' value='all'><param name='allowFullScreen' value='true'><param name='flashvars' value='channel=#{chan}&amp;auto_play=false&amp;start_volume=25&amp;chapter_id=#{id}'></object><br><a href='http://www.twitch.tv/#{chan}' class='trk' style='padding:2px 0px 4px; display:block; width: 320px; font-weight:normal; font-size:10px; text-decoration:underline; text-align:center;'>Watch live video from #{chan} on TwitchTV</a></div>
         <div class='zemusnoflash'>Your browser cannot display this embedded Twitch stream.</div>"
       else 
         chan = @url.split('/').fourth
