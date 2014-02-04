fbf5ace7cc91a71666917a97be6e0a322eb938a8
Travis Newman <travis.c.newman@gmail.com>
2014-02-03 22:54:56 -0500

    fixed spacing issue in twitch.rb

--- lib/zemus/twitch.rb
+++ lib/zemus/twitch.rb
@@ -11,7 +11,7 @@ module Zemus
 
     def to_embed
       id = @url.split('/').last
-      if     @url.split('/').fifth == "b"
+      if @url.split('/').fifth == "b"
         chan = @url.split('/').fourth
         "<div class='zemusflashembed'><object data='http://www.twitch.tv/widgets/archive_embed_player.swf' height='378' id='clip_embed_player_flash' type='application/x-shockwave-flash' ><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf'><param name='allowScriptAccess' value='always'><param name='allowNetworking' value='all'><param name='allowFullScreen' value='true'><param name='flashvars' value='channel=#{chan}&amp;auto_play=false&amp;start_volume=25&amp;archive_id=#{id}'></object></div>
         <div class='zemusnoflash'><a href='#{@url}'>Your browser cannot display this embedded Twitch video</a></div>"
